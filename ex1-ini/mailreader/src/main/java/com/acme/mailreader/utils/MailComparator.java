85b2d8ad1caf7856add242ddf52f6054ce03cfa1
Quentin MALINGE <quentin.malinge@gmail.com>
2019-03-11 08:58:15 +0100

    Remplace les entiers retours (0, 1, -1) par des constantes plus parlantes

--- ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
+++ ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
@@ -11,22 +11,26 @@ import com.acme.mailreader.model.Mail;
  *
  */
 public class MailComparator implements Comparator<Mail> {
-
+	
+	public static final int MAILS_EQUALS = 0;
+	public static final int FIRST_SMALLER = 1;
+	public static final int FIRST_BIGGER = -1;
+	
 	public int compare(Mail obj1, Mail obj2) {
 		if (obj1 == null || obj2 == null) {
-			return 0;
+			return MAILS_EQUALS;
 		}
 		if (obj1.isImportant() != obj2.isImportant()) {
 			if (obj1.isImportant() && !obj2.isImportant()) {
-				return -1;
+				return FIRST_BIGGER;
 			} else {
-				return 1;
+				return FIRST_SMALLER;
 			}
 		}
 		if (obj1.getStatut() != obj2.getStatut()) {
 			int comp = obj1.getStatut().ordinal()
 					- obj2.getStatut().ordinal();
-			return comp > 0 ? -1 : 1;
+			return comp > 0 ? FIRST_BIGGER : FIRST_SMALLER;
 		}
 		if (obj1.getSujet() != obj2.getSujet()) {
 			return obj2.getSujet().compareTo(obj1.getSujet());
@@ -34,5 +38,4 @@ public class MailComparator implements Comparator<Mail> {
 		return obj2.getDate().compareTo(obj1.getDate());
 	}
 	
-
 }
